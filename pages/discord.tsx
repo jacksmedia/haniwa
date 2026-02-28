6da79921ba064e1c17f0d5f5ca460fff32f7e271
jacksmedia <alex@j4cks.com>
2026-02-27 16:23:02 -0800

    🏁MVP, v0.9 patch docs added

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -6,10 +6,11 @@ const Discord: NextPage = () => {
   return (
     <Layout>      
       <div className='discord-bg'>
+        <h2>Join us in Discord</h2>
         <TitleScreen />
         <h1 className="">Discuss SaGa2 AHC</h1>
-        <h2>#lightning-projects</h2>        
-        <h2><a href="https://discord.gg/PGMASbSnD9" target='blank'>Join us in the FF4 Ultima Discord</a></h2>
+        <h2>( #lightning-projects channel )</h2>        
+        <h2><a href="https://discord.gg/PGMASbSnD9" target='blank'>▶ FF4 Ultima Discord ◀</a></h2>
       </div>
     </Layout>
   );
